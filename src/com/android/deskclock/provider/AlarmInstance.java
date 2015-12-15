a8040c99d2baea2341172928c437b4af22b64702
Wilhelm Fitzpatrick <rafial@cyngn.com>
2015-12-15 13:45:06 -0800

    DeskClock: add increasing volume option for alarm clocks
    Adapted for CandyRoms by NoSpamDan
    
    Based on CM12 feature:
    
    https://github.com/CyanogenMod/android_packages_apps_DeskClock/commit/7fc93e1b93b0e9e56a8426a6201a26c2bfca90ba
    http://review.cyanogenmod.org/#/c/82967/
    
    Conflicts:
    	res/values/cm_strings.xml
    
    Change-Id: I090c76bd773e419f10e1e02bcf3c40ad04d6a988

--- src/com/android/deskclock/provider/AlarmInstance.java
+++ src/com/android/deskclock/provider/AlarmInstance.java
@@ -71,7 +71,8 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
             VIBRATE,
             RINGTONE,
             ALARM_ID,
-            ALARM_STATE
+            ALARM_STATE,
+            INCREASING_VOLUME,
     };
 
     /**
@@ -89,8 +90,9 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
     private static final int RINGTONE_INDEX = 8;
     private static final int ALARM_ID_INDEX = 9;
     private static final int ALARM_STATE_INDEX = 10;
+    private static final int INCREASING_VOLUME_INDEX = 11;
 
-    private static final int COLUMN_COUNT = ALARM_STATE_INDEX + 1;
+    private static final int COLUMN_COUNT = INCREASING_VOLUME_INDEX + 1;
 
     public static ContentValues createContentValues(AlarmInstance instance) {
         ContentValues values = new ContentValues(COLUMN_COUNT);
@@ -114,6 +116,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
         }
         values.put(ALARM_ID, instance.mAlarmId);
         values.put(ALARM_STATE, instance.mAlarmState);
+        values.put(INCREASING_VOLUME, instance.mIncreasingVolume ? 1 : 0);
         return values;
     }
 
@@ -333,6 +336,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
     public Uri mRingtone;
     public Long mAlarmId;
     public int mAlarmState;
+    public boolean mIncreasingVolume;
 
     public AlarmInstance(Calendar calendar, Long alarmId) {
         this(calendar);
@@ -346,6 +350,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
         mVibrate = false;
         mRingtone = null;
         mAlarmState = SILENT_STATE;
+        mIncreasingVolume = false;
     }
 
     public AlarmInstance(Cursor c) {
@@ -369,6 +374,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
             mAlarmId = c.getLong(ALARM_ID_INDEX);
         }
         mAlarmState = c.getInt(ALARM_STATE_INDEX);
+        mIncreasingVolume = c.getInt(INCREASING_VOLUME_INDEX) == 1;
     }
 
     public String getLabelOrDefault(Context context) {
@@ -480,6 +486,7 @@ public final class AlarmInstance implements ClockContract.InstancesColumns {
                 ", mRingtone=" + mRingtone +
                 ", mAlarmId=" + mAlarmId +
                 ", mAlarmState=" + mAlarmState +
+                ", mIncreasingVolume=" + mIncreasingVolume +
                 '}';
     }
 }
